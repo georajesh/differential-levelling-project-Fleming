b074577f76df10d0a781ff523794dd44e713b6b0
Rajesh Dulal <146376153+georajesh@users.noreply.github.com>
2023-11-11 00:17:25 -0500

    Update Demo_Function.py

--- Demo_Function.py
+++ Demo_Function.py
@@ -1,37 +1,30 @@
-#Function definition
-def calculate_elevation (backsight, foresight, elevation):
-    elevation = elevation + backsight - foresight
-    
-    return elevation
-
-# try:
 # Display program purpose
 print("This application calculates and checks the elevation of differential survey data.")
 print()
 print('************************************************************') 
 print()
 
-table_data = {'back_sight' : 0, 'fore_sight' : 0, 'height': 0}
+table_data = {'backsight' : None, 'foresight' : None, 'elevation' : None}
 benchmark = float(input("Enter the elevation of benchmark: "))
 #Using indefinite Lop
 while True:
+    station = str(input("Enter the station name: "))
+
     back_sight = float(input("Enter the backsight reading: "))
-    table_data[back_sight] += 1
 
     fore_sight = float(input("Enter the foresight reading: "))
-    table_data[fore_sight] += 1
+    
+    elevation = benchmark + back_sight - fore_sight
+    
+    key = len(table_data) + 1
+    table_data[key] = {'backsight': back_sight, 'foresight': fore_sight, 'elevation': elevation}
 
-    height = calculate_elevation (back_sight, fore_sight, height)
-    table_data[height] +=1
-
-
+    benchmark = elevation
     answer = input("Do you want to enter another station? (Y/N)")
     answer = answer.upper()
     if answer == "N":
         break
-
+print("Station\t Backsight\t Foresight\t Elevation")
+for item in {'backsight'}:
+    print(f"{station}\t {back_sight}\t\t {fore_sight}\t\t {elevation}")
         
-# #Except specific Exception
-# #General Exception
-# except Exception as message:
-#     print("Error: "+ message)
